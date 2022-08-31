f2cbda2d4ef9cbae1d0dc60eb236486910f6758f
Owen Nicoll <2146863@fife.ac.uk>
2022-08-31 13:27:24 +0100

    Practiced variable types

--- Cplusplus Practoce Project/Cplusplus Practoce Project.cpp
+++ Cplusplus Practoce Project/Cplusplus Practoce Project.cpp
@@ -2,10 +2,23 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Variable Types Examples
+    int testInt = 100;
+    unsigned testUnsigned = 100u;
+    float testFloat = 0.5f;
+    double testDouble = 0.5;
+    bool testBool = true;
+    char testChar = 'b';
+
+    std::string testString = "No";
+
+    // Console Output Example
+    std::cout << "Hello World!\n"
+        << testString;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu

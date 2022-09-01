47d776a7f5b1748dc7af6744077d08dac3704a63
OwenNicoll <100194096+OwenNicoll@users.noreply.github.com>
2022-09-01 01:15:21 +0100

    Completed practice exercises 1-5

--- Cplusplus Practoce Project/Cplusplus Practoce Project.cpp
+++ Cplusplus Practoce Project/Cplusplus Practoce Project.cpp
@@ -6,80 +6,174 @@
 
 int main()
 {
-    // Variable Types Examples
-    int testInt = 100;
-    unsigned testUnsigned = 100u;
-    float testFloat = 0.5f;
-    double testDouble = 0.5;
-    bool testBool = true;
 
-    char testChar = 'b';
-    char firstNameOld[] = "Owen";
+        // EXCERCISE 1
+    
+     //Declare variables
+    int perimeter = 0;
+    int area = 0;
+    int length = 0;
+    int width = 0;
 
-    std::string firstName = "Owen";
-    std::string lastName = "Nicoll";
+    // User input
+    std::cout << "Enter the length of a rectangle \n";
+    std::cin >> length;
+    std::cout << "Enter the width of the rectangle \n";
+    std::cin >> width;
 
-    std::string fullName = firstName + " " + lastName;
+    // Calculations
+    perimeter = 2 * (length + width);  
+    area = length * width;
 
-    float result = 50 % 100;
-
-    std::string testString = "No";
-
-    char firstInitial = fullName[0];
-
-    std::string initials = fullName.substr(0, 1);
-
-    // Find the position in the string and store it in a variable of type size_t (like an int)
-    std::size_t spacePos = fullName.find(" ");
-    initials += fullName.substr(spacePos + 1, 1);
+    // Output
+    std::cout << "Area = " << area << "\n"
+        << "Perimeter = " << perimeter << "\n";
 
     
-    std::string paragraph = "A paragraph (from Ancient Greek παράγραφος (parágraphos) 'to write beside') is a self-contained unit of discourse in writing dealing with a particular point or idea.";
+        // EXCERCISE 2
 
-    paragraph = paragraph.substr(0, 20);
+    // Declare variables
+    int timeHours = 0;
+    int timeMinutes = 0;
+    int timeSeconds = 0;
+
+    // User input
+    std::cout << "Enter a time in hours: \n";
+    std::cin >> timeHours;
+
+    // Calculations
+    timeMinutes = timeHours * 60;
+    timeSeconds = timeMinutes * 60;
+
+    // Output
+    std::cout << "Time in minutes = " << timeMinutes << "\n"
+        << "Time in seconds = " << timeSeconds << "\n";
+        
+
+
+        // EXCERCISE 3
 
     
-    // USER INPUT
-    std::cout << "Please enter a word. \n";
+    std::string insultOne = "You're a ";
+    std::string insultTwo = "Donkey \n";
 
-    // A variable to store the user input
-    std::string entryString;
+    // Combine the two strings
+    std::string insultFull = insultOne + insultTwo;
 
-    // Get user input
-    std::cin >> entryString;
-
-    // Print the user's word
-    std::cout << "You typed: " << entryString << "!\n";
-
-    // We can also input things other than strings
-    int entryInt;
-    std::cout << "Please enter a whole number. \n";
-    std::cin >> entryInt;
-    std::cout << "You typed: " << entryInt << "\n";
+    // Print the combined string
+    std::cout << insultFull;
 
 
-    float entryFloat;
-    std::cout << "Enter a number with a decimal place \n";
-    std::cin >> entryFloat;
-    std::cout << "You typed " << entryFloat << "\n";
-    
-    std::string lineOfText;
-    std::cout << "Please enter a line of text \n";
-    std::getline(std::cin, lineOfText);
-    std::cout << lineOfText;
+        // EXCERCISE 4
+
+    // Declare variables
+    std::string codeWord = "codeword";
+    std::string sentance = "Find the codeword in this sentance";
+
+    // Find position of word within the sentance
+    std::size_t wordPos = sentance.find(codeWord);
+
+    // Print the position
+    std::cout << wordPos;
+
+        // EXCERCISE 5
+
+    // Declare variables
+    int year = 0;
+    int day = 0;
+    std::string month;
+
+    // User input
+    std::cout << "Enter your year of birth: \n";
+    std::cin >> year;
+
+    std::cout << "Enter your month of birth: \n";
+    std::cin >> month;
+
+    std::cout << "Enter your day of birth: \n";
+    std::cin >> day;
+
+    // Print full birthday
+    std::cout << "Your birthday is the " << day << " of " << month << " " << year;
 
 
 
-    // SELECTION
-    bool decision = true;
-    if (decision == true)
-    {
-        std::cout << "Decision was true! \n";
-    }
-    else
-    {
-        std::cout << "Decision was false \n";
-    }
+
+
+    //// Variable Types Examples
+    //int testInt = 100;
+    //unsigned testUnsigned = 100u;
+    //float testFloat = 0.5f;
+    //double testDouble = 0.5;
+    //bool testBool = true;
+
+    //char testChar = 'b';
+    //char firstNameOld[] = "Owen";
+
+    //std::string firstName = "Owen";
+    //std::string lastName = "Nicoll";
+
+    //std::string fullName = firstName + " " + lastName;
+
+    //float result = 50 % 100;
+
+    //std::string testString = "No";
+
+    //char firstInitial = fullName[0];
+
+    //std::string initials = fullName.substr(0, 1);
+
+    //// Find the position in the string and store it in a variable of type size_t (like an int)
+    //std::size_t spacePos = fullName.find(" ");
+    //initials += fullName.substr(spacePos + 1, 1);
+
+    //
+    //std::string paragraph = "A paragraph (from Ancient Greek παράγραφος (parágraphos) 'to write beside') is a self-contained unit of discourse in writing dealing with a particular point or idea.";
+
+    //paragraph = paragraph.substr(0, 20);
+
+    //
+    //// USER INPUT
+    //std::cout << "Please enter a word. \n";
+
+    //// A variable to store the user input
+    //std::string entryString;
+
+    //// Get user input
+    //std::cin >> entryString;
+
+    //// Print the user's word
+    //std::cout << "You typed: " << entryString << "!\n";
+
+    //// We can also input things other than strings
+    //int entryInt;
+    //std::cout << "Please enter a whole number. \n";
+    //std::cin >> entryInt;
+    //std::cout << "You typed: " << entryInt << "\n";
+
+
+    //float entryFloat;
+    //std::cout << "Enter a number with a decimal place \n";
+    //std::cin >> entryFloat;
+    //std::cout << "You typed " << entryFloat << "\n";
+    //
+    //std::string lineOfText;
+    //std::cout << "Please enter a line of text \n";
+    //std::getline(std::cin, lineOfText);
+    //std::cout << lineOfText;
+
+
+    //
+    //// SELECTION
+    //bool decision = true;
+    //if (decision == true)
+    //{
+    //    std::cout << "Decision was true! \n";
+    //}
+    //else
+    //{
+    //    std::cout << "Decision was false \n";
+    //}
 }
 
 
